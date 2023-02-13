635a2b3a913dcd3510e896813f5afd1468e69e2b
NinoSayo <119102188+NinoSayo@users.noreply.github.com>
2023-02-13 23:16:35 +0700

    V1

--- route.js
+++ route.js
@@ -9,5 +9,14 @@ app.config(function($routeProvider){
     .when("/login",{templateUrl:"login.html"})
     .when("/cart",{templateUrl:"cart.html"})
     .when("/product1",{templateUrl:"product1.html"})
+    .when("/product2",{templateUrl:"product2.html"})
+    .when("/product3",{templateUrl:"product3.html"})
+    .when("/product4",{templateUrl:"product4.html"})
+    .when("/product5",{templateUrl:"product5.html"})
+    .when("/product6",{templateUrl:"product6.html"})
+    .when("/product7",{templateUrl:"product7.html"})
+    .when("/product8",{templateUrl:"product8.html"})
+    .when("/product9",{templateUrl:"product9.html"})
+    .when("/product10",{templateUrl:"product10.html"})
 });
 
